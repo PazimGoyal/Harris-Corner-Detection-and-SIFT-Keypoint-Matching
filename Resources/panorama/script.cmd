e6767a27f58d2b5868860286d24dc317f92ed15e
Pazim Goyal <pazimgoyal@gmail.com>
2019-02-14 19:48:51 -0500

    upload ready

--- Resources/panorama/script.cmd
+++
@@ -1,3 +0,0 @@
-run alignPair warp08.key warp09.key match-08-09.txt 100 4.0 sift
-run alignPair warp09.key warp10.key match-09-10.txt 100 4.0 sift
-run alignPair warp10.key warp11.key match-10-11.txt 100 4.0 sift
